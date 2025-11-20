80d64b003934bb1664d58552edd073e9a05b73c9
Carlos-RT <carlos.ramirez02@unicatolica.edu.co>
2025-11-20 14:18:24 -0500

    correccion de una letra

--- Mi_Moneda/src/components/Inicio.jsx
+++ Mi_Moneda/src/components/Inicio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import fondoinicio from "../imagenes/Fondo_inicio.jpg";
+import fondoinicio from "../imagenes/fondo_inicio.jpg";
 
 export default function Inicio() {
   const navigate = useNavigate();
